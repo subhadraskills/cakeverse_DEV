8e49698346e0bd969bc65bb24c2a11d4f0604890
Subhadra Kumari <subhadraskills@gmail.com>
2025-03-21 09:27:33 +0530

    Add new feature

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config(); 
-}
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -17,132 +14,83 @@ const flash = require("connect-flash");
 
 const User = require("./models/user.js");
 const cakeRouter = require("./routes/cake.js");
-const reviewRouter =require("./routes/review.js");
+const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-
-
-
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/cakeverse";
-
-const dbUrl = process.env.ATLASDB_URL;  
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const dbUrl = process.env.ATLASDB_URL;
 
 
 main()
-    .then(() => {
-        console.log("connected to DB");
-    })
-    .catch((err) => {
-        console.log("Error connecting to DB: ", err);
-    });
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log("DB connection error:", err));
+async function main() {
+  await mongoose.connect(dbUrl);
+}
 
 
-  async function main(){
-await mongoose.connect(dbUrl);
-  }
-
-  // Middleware
- 
-  app.set("view engine", "ejs");
-  app.set("views", path.join(__dirname, "views"));
-  app.use(express.urlencoded({extended: true}));
-  app.use(methodOverride("_method"));
-  app.engine("ejs", ejsMate);
-  app.use(express.static(path.join(__dirname, "/public")));
-
-
-
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  crypto: {
-      secret: process.env.SECRET,
-  },
-  touchAfter: 24 * 60 *60,
+  crypto: { secret: process.env.SECRET },
+  touchAfter: 24 * 60 * 60,
 });
-
-
-
-store.on("error", () => {
-  console.log("ERROR IN MONGO SESSION STORE", err);
-});
-
-
-
-// Session configuration
+store.on("error", (err) => console.log("Session store error:", err));
 const sessionOptions = {
   store,
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie:
-  {
-      expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-      maxAge: 7 * 24 * 60 *60 * 1000,
-      httpOnly: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
   },
 };
-
-
 app.use(session(sessionOptions));
 app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
 
-// Passport configuration
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// Flash messages and current user middleware
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
+  res.locals.currUser = req.user; 
   next();
 });
 
-// Routes
+
 app.use("/cakes", cakeRouter);
 app.use("/cakes/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
 
-
-app.post("/login", passport.authenticate("local", {
-  failureRedirect: "/login",
-  failureFlash: "Invalid username or password"
-}), (req, res) => {
- 
-  res.redirect("/dashboard");
-});
-
-
-
-
-app.all("*", (req,res, next) => {
+app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
-})
-
+});
 
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = "Something went wrong!" } = err;
-
   if (res.headersSent) {
-      return next(err);  
+    return next(err);
   }
   res.status(statusCode).render("error.ejs", { message });
 });
 
 
-
-
-//start the server
-app.listen(5000, ()=>{
-    console.log("server is listing to port 5000"
-      );
-});
+app.listen(5000, () => console.log("Server running on port 5000"));
